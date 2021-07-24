0dffb029e65e63d2f17d203725d374650ab69b98
Tony Ding <62191432+djq060912@users.noreply.github.com>
2021-07-24 08:41:56 +0800

    arrow-ify

--- arrow_pdx206.mk
+++ arrow_pdx206.mk
@@ -23,17 +23,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Pixel Experience stuff
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
+ARROW_GAPPS := true
 IS_PHONE := true
 
 PRODUCT_BRAND := Sony
 PRODUCT_DEVICE := pdx206
 PRODUCT_MANUFACTURER := Sony
 
-PRODUCT_NAME := dot_pdx206
+PRODUCT_NAME := arrow_pdx206
 PRODUCT_MODEL := XQ-AS72
 
 
@@ -41,6 +41,13 @@ PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
 
 TARGET_VENDOR_PRODUCT_NAME := pdx206
 
+# Fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210705.001 7380771 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="Sony/pdx206/pdx2062 10 QKQ1.200108.002 1:user release-keys"
-BUILD_FINGERPRINT := Sony/pdx206/pdx206:10/QKQ1.200108.002/1:user/release-keys
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
+
